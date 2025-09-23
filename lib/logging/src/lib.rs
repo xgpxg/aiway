05031bf2b4e949ff939ab7b9445db5669e52ece9
wxg <1584929962@qq.com>
2025-09-23 16:30:41 +0800

    logging: re-export log

--- lib/logging/src/lib.rs
+++ lib/logging/src/lib.rs
@@ -17,6 +17,7 @@ use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
 use tracing_subscriber::{Registry, fmt};
 
+pub use log;
 mod writer;
 
 static LOG_GUARD: OnceLock<tracing_appender::non_blocking::WorkerGuard> = OnceLock::new();
--- lib/logging/src/writer/http_writer.rs
+++ lib/logging/src/writer/http_writer.rs
@@ -19,16 +19,13 @@ impl HttpLogWriter {
 
 impl std::io::Write for HttpLogWriter {
     fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
-        let message = String::from_utf8_lossy(buf).to_string();
+        let message = buf.to_vec();
         let endpoint = self.endpoint.clone();
         let client = self.client.clone();
 
         tokio::spawn(async move {
-            match client.get(&endpoint).body(message.clone()).send().await {
-                Ok(_) => {
-                    //println!("write log to http endpoint success: {}",message)
-                }
-                Err(e) => println!("write log to http endpoint error: {}", e),
+            if let Err(e) = client.post(&endpoint).body(message).send().await {
+                println!("write log to http endpoint error: {}", e);
             }
         });
 
