b536fc45f2d14f876104f5da629d568f90b0d1ef
wxg <1584929962@qq.com>
2025-12-11 15:51:57 +0800

    fix bin name

--- aiway/src/main.rs
+++ aiway/src/main.rs
@@ -3,6 +3,7 @@ mod embed;
 use cache::start_share_cache_server;
 use logging::{init_log, log};
 use rust_embed::Embed;
+use std::ops::Deref;
 use std::thread::sleep;
 use std::time::Duration;
 
@@ -24,17 +25,27 @@ struct AiwayApp {
 
 impl AiwayApp {
     fn new() -> Self {
-        let console = Asset::get("console").unwrap();
-        let gateway = Asset::get("gateway").unwrap();
-        let logg = Asset::get("logg").unwrap();
-        let logg = embed::EmbedApp::new("logg", &logg.data, &[]).unwrap();
+        let console = Asset::get(BinaryName("console").as_ref()).unwrap();
+        let gateway = Asset::get(BinaryName("gateway").as_ref()).unwrap();
+        let logg = Asset::get(BinaryName("logg").as_ref()).unwrap();
+        let logg = embed::EmbedApp::new(BinaryName("logg").as_ref(), &logg.data, &[]).unwrap();
         log::info!("log server started");
-        let console = embed::EmbedApp::new("console", &console.data, &["--log-server","127.0.0.1:7281"]).unwrap();
+        let console = embed::EmbedApp::new(
+            BinaryName("console").as_ref(),
+            &console.data,
+            &["--log-server", "127.0.0.1:7281"],
+        )
+        .unwrap();
         log::info!("console started");
         // 等待console启动完成
         // 这里实现 不优雅，先这样，后续处理
         sleep(Duration::from_secs(2));
-        let gateway = embed::EmbedApp::new("gateway", &gateway.data, &["--log-server","127.0.0.1:7281"]).unwrap();
+        let gateway = embed::EmbedApp::new(
+            BinaryName("gateway").as_ref(),
+            &gateway.data,
+            &["--log-server", "127.0.0.1:7281"],
+        )
+        .unwrap();
         log::info!("gateway started");
         AiwayApp {
             logg,
@@ -44,9 +55,24 @@ impl AiwayApp {
     }
 }
 
+pub struct BinaryName<'a>(&'a str);
+impl std::fmt::Display for BinaryName {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if cfg!(windows) {
+            write!(f, "{}.exe", self.0)
+        } else {
+            write!(f, "{}", self.0)
+        }
+    }
+}
+
+impl AsRef<str> for BinaryName {
+    fn as_ref(&self) -> &str {
+        self.0
+    }
+}
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-
     init_log();
     tokio::spawn(async {
         start_share_cache_server("cache").await.unwrap();
