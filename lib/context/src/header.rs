4c74b023f453a80d69f6aceb12fc4705c21d4674
wxg <1584929962@qq.com>
2026-01-16 00:01:28 +0800

    style(header): header name转小写

--- lib/context/src/header.rs
+++ lib/context/src/header.rs
@@ -2,14 +2,14 @@ use rocket::Request;
 
 pub struct Headers;
 impl Headers {
-    pub const REQUEST_ID: &'static str = "X-Aiway-Request-Id";
-    pub const REQUEST_TIME: &'static str = "X-Aiway-Request-Time";
-    pub const AUTHORIZATION: &'static str = "Authorization";
-    pub const ERROR_CODE: &'static str = "X-Error-Code";
-    pub const ERROR_MESSAGE: &'static str = "X-Error-Message";
-    pub const REFERER: &'static str = "Referer";
-    pub const USER_AGENT: &'static str = "User-Agent";
-    pub const CONTENT_TYPE: &'static str = "Content-Type";
+    pub const REQUEST_ID: &'static str = "x-aiway-request-id";
+    pub const REQUEST_TIME: &'static str = "x-aiway-request-time";
+    pub const AUTHORIZATION: &'static str = "authorization";
+    pub const ERROR_CODE: &'static str = "x-error-code";
+    pub const ERROR_MESSAGE: &'static str = "x-error-message";
+    pub const REFERER: &'static str = "referer";
+    pub const USER_AGENT: &'static str = "user-agent";
+    pub const CONTENT_TYPE: &'static str = "content-type";
 }
 
 impl Headers {
